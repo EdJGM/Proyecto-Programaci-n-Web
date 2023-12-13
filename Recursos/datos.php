18134339fb6f9005194770d178c9b7d5fa569eeb
Edgar <nicolasmen91@gmail.com>
2023-12-12 23:42:02 -0500

    completov1

--- Recursos/datos.php
+++ Recursos/datos.php
@@ -1,27 +1,59 @@
-<!--
-<hr>
-<h2>METODO GET </h2>;
-    echo "<strong>Nombre registrado: </strong>".$_GET['name']."<br>";
-    echo "<strong>Apellido registrado: </strong>".$_GET["lastname"]."<br>";
-    echo "<strong>Fecha registrada: </strong>".$_GET["date"]."<br>";
-    echo "<strong>Email registrado: </strong>".$_GET["email"]."<br>";
-    echo "<strong>Contrasena registrada: </strong>".$_GET["password"]."<br>";    
--->
-<hr>
-<h2>METODO POST </h2>
-<?php     
-    echo "<strong>Usuario: </strong>".$_POST['username']."<br>";
-    echo "<strong>Contrasena: </strong>".$_POST["password"]."<br>";
-?>
-<!--
-<hr>
-<h2>METODO POST </h2>;
-    $numero1 = $_POST['number1'];
-    $numero2 = $_POST['number2'];        
-    echo "<strong>Numero 1: </strong>".$_POST['number1']."<br>";
-    echo "<strong>Numero 2: </strong>".$_POST["number2"]."<br>";
-    echo "Numeros aleatorios entre [".$numero1." , ".$numero2. "]: <br>";
-    for ($i = 0; $i < 10; $i++) {
-        echo rand($numero1,$numero2)."<br>";
+<?php session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Formulario de Registro
+    if (isset($_POST['name']) && isset($_POST['lastname']) && 
+        isset($_POST['date']) && isset($_POST['email']) && 
+        isset($_POST['password'])) {
+        $nombre = $_POST['name'];
+        $apellido = $_POST['lastname'];
+        $fechaNacimiento = $_POST['date'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        echo '<h2>Datos recibidos del Registro</h2>';
+        echo '<table border="1">';
+            echo '<tr>';
+                echo '<th>Nombre</th>';
+                echo '<th>Apellido</th>';
+                echo '<th>Fecha de Nacimiento</th>';
+                echo '<th>Email</th>';
+                echo '<th>Contraseña</th>';
+            echo '</tr>';
+            echo '<tr>';
+                echo '<td>'.$nombre.'</td>';
+                echo '<td>'.$apellido.'</td>';
+                echo '<td>'.$fechaNacimiento.'</td>';
+                echo '<td>'.$email.'</td>';
+                echo '<td>'.$password.'</td>';
+            echo '</tr>';
+        echo '</table>';
+
     }
--->
+
+    // Formulario de Inicio de Sesión
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $nombreUsuario = $_POST['username'];
+        $contrasena = $_POST['password'];
+        echo '<h2>Datos recibidos del LogIn</h2>';
+        echo '<table border="1">';
+            echo '<tr>';
+                echo '<th>Nombre de Usuario</th>';
+                echo '<th>Contraseña</th>';
+            echo '</tr>';
+            echo '<tr>';
+                echo '<td>'.$nombreUsuario.'</td>';
+                echo '<td>'.$contrasena.'</td>';
+            echo '</tr>';
+        echo '</table>';
+        echo '<br>';
+        // Verificar las credenciales (¡NO usar en producción!)
+        if ($nombreUsuario === 'usuario' && $contrasena === 'contrasena') {
+            // Redirigir o realizar acciones después del inicio de sesión exitoso
+            header('Location: PaginaLogin.html');
+            exit();
+        } else {
+            echo "Login failed. Please check your credentials.";
+        }        
+    }
+}
+?>
