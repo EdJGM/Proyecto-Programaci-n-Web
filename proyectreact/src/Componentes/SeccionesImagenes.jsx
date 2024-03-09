7bb2b51199e00e646e7a44b949a99536e4c994b4
Edgar <nicolasmen91@gmail.com>
2024-03-09 01:04:31 -0500

    Refactor image section component to use React Router for navigation

--- proyectreact/src/Componentes/SeccionesImagenes.jsx
+++ proyectreact/src/Componentes/SeccionesImagenes.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function SeccionesImagenes(){
-    function handleButtonClick(url){
+function SeccionesImagenes() {
+    function handleButtonClick(url) {
         window.location.href = url;
-    };    
+    };
 
     return (
         <section className="image-section-container">
             <div className="image-container">
-                <img src="./Img/retrato-joven-alegre-ordenador-portatil.jpg" alt="Imagen 1"/>
+                <img src="./Img/retrato-joven-alegre-ordenador-portatil.jpg" alt="Imagen 1" />
                 <div className="texto-container">
-                    <p className="texto1-descuento">40% <br/> VENTA</p>
+                    <p className="texto1-descuento">40% <br /> VENTA</p>
                     <p className="texto2-nombre">Laptop</p>
                     <p className="texto3-categoria">Laptops</p>
-                    <button id="irLaptops" className="add-to-cart-button" onClick={() => handleButtonClick('./Categorias/PaginaLaptops.html')}>Ir Ya!</button>
+                    <Link to="/categoria/laptops"><button id="irLaptops" className="add-to-cart-button">Ir Ya!</button></Link>
                 </div>
             </div>
             <div className="image-container">
-                <img src="./Img/exhibicion-auriculares-musica-levitando.jpg" alt="Imagen 1"/>
+                <img src="./Img/exhibicion-auriculares-musica-levitando.jpg" alt="Imagen 1" />
                 <div className="texto-container">
-                    <p className="texto1-descuento">60% <br/> VENTA</p>
+                    <p className="texto1-descuento">60% <br /> VENTA</p>
                     <p className="texto2-nombre">Audifonos</p>
                     <p className="texto3-categoria">Accesorios</p>
-                    <button id="irAccesorios" className="add-to-cart-button" onClick={() => handleButtonClick('./Categorias/PaginaAccesorios.html')}>Ir Ya!</button>
+                    <Link to="/categoria/accesorios"><button id="irAccesorios" className="add-to-cart-button">Ir Ya!</button></Link>
                 </div>
             </div>
             <div className="image-container">
-                <img src="./Img/retrato-atractiva-mujer-moda-complacida-gafas-blusa-mostrando-pantalla-telefono-apuntando-al-dispositivo-dedo-indice-sonriendo-ampliamente.jpg" alt="Imagen 1"/>
+                <img src="./Img/retrato-atractiva-mujer-moda-complacida-gafas-blusa-mostrando-pantalla-telefono-apuntando-al-dispositivo-dedo-indice-sonriendo-ampliamente.jpg" alt="Imagen 1" />
                 <div className="texto-container">
-                    <p className="texto1-descuento">10% <br/> VENTA</p>
+                    <p className="texto1-descuento">10% <br /> VENTA</p>
                     <p className="texto2-nombre">Celulares</p>
                     <p className="texto3-categoria">Telefonía</p>
-                    <button id="irCelulalres" className="add-to-cart-button" onClick={() => handleButtonClick('./Categorias/PaginaCelulares.html')}>Ir Ya!</button>
+                    <Link to="/categoria/celulares"><button id="irCelulalres" className="add-to-cart-button" >Ir Ya!</button></Link>
                 </div>
             </div>
         </section>
